fe4721849959c415a3a49612ed55940a5eb650a6
Tomás Weigenast <tomaswegenast@gmail.com>
2022-12-01 17:39:23 -0300

    update publish.sh

--- publish.sh
+++ publish.sh
@@ -6,4 +6,5 @@ git add .
 git commit -m "automate: changes for v$version"
 git tag v$version
 git push origin v$version
+GOPROXY=proxy.golang.org go list -m github.com/messagepack-schema/go@v$version
 echo "version published."
